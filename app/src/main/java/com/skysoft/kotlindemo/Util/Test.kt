50a23403ccc0394f15346b1ff843e62133dc1788
ben.zhao <ben.zhao@cdskysoft.com>
2020-07-29 11:33:28 +0800

    clear

--- app/src/main/java/com/skysoft/kotlindemo/Util/Test.kt
+++ app/src/main/java/com/skysoft/kotlindemo/Util/Test.kt
@@ -1,20 +1,5 @@
 package com.skysoft.kotlindemo.Util
 
 class Test {
-    //我要开始了
-    //开始了
-    //开始了吗
-    //SB
-    //66666
-    //gg
-    //我又提交了
-    //我提交了
-    //nishige
-    //灌灌灌灌
-    //主分支提交
-    //feature
-    //主分支又提交了
-    //主分支又提交了
-    //feature提交了
-    //主分支
+ 
 }
